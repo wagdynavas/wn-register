5747561d85da4abcbd912a0077b911a5fac547bd
Wagdy Navas <dinholelles@gmail.com>
2019-04-18 16:09:05 -0300

    Initial commit

---
+++ src/main/java/com/wagdynavas/wnregister/WnRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.wnregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wagdynavas/wnregister/WnRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wagdynavas.wnregister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WnRegisterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
